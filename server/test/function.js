3609c2fcd18fadac64e575b8b9f9d54574baef19
tianzx <tianzx@aliyun.com>
2017-03-24 17:49:07 +0800

    convert mysql datetime to client receive format

--- server/test/function.js
+++ server/test/function.js
@@ -2,21 +2,43 @@
  * Created by tianzx on 2017/3/24.
  */
 const file = require('../../fake/file');
-const _ =  require('lodash');
+const _ = require('lodash');
 const moment = require('moment');
 
 
-const convertTime = function(val){
-  return val.createTime = moment.unix(val);
-};
-
-// const cv_file = _.map(file,convertTime);
-
-
-_.each(file,function (value) {
-  console.log(moment.unix(value.createTime/1000).format("YYYY-MM-DD HH:mm:ss"));
+/**
+ * before convert datetime
+ */
+_.each(file, function (value) {
+  console.log(value.createTime);
 });
 
-_.each(file,function (value) {
+/**
+ * convert datetime
+ */
+_.map(file, function (value) {
+  value.createTime = moment.unix(value.createTime / 1000).format("YYYY-MM-DD HH:mm:ss");
+  console.log(value.createTime);
+});
+
+/**
+ * convert datetime no side effects
+ * @type {Array}
+ */
+const file2 = _.map(file,function(value){
+  const temp = _.cloneDeep(value);
+  console.log(temp);
+  temp.createTime = moment.unix(temp.createTime / 1000).format("YYYY-MM-DD HH:mm:ss");
+  return temp;
+});
+
+/**
+ * after convert datetime
+ */
+_.each(file, function (value) {
+  // console.log(value.createTime);
+});
+
+_.each(file2, function (value) {
   console.log(value);
 });
