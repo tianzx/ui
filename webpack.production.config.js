75997cf1294267763270cbfc50c34f91ba95954c
tianzx <tianzx@aliyun.com>
2017-04-01 16:50:46 +0800

    Analytics pg.json

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,18 +2,16 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const HappyPack = require('happypack');
+const HappyPack = require('happypack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: {
     bundle: './src/index',
-    vendor: ['react']
+    vendor: ['react', 'lodash','antd']
   },
   output: {
     path: __dirname + '/dist',
-    // path: path.join(__dirname, 'dist'),
-    // filename: 'bundle-[chunkhash:6].js',
-    filename: "[name].chunkhash.js",
+    filename: "[name].[chunkhash:6].js",
     chunkFilename: "[chunkhash].js",
     publicPath: './'
   },
@@ -32,55 +30,63 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    // new webpack.optimize.DedupePlugin(),
+
     new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
       compress: {
-        warnings: false,
-        drop_console: true
+        unused: true,
+        dead_code: true, // big one--strip code that will never execute
+        warnings: false, // good for prod apps so users can't peek behind curtain
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true, // strips console statements
+        sequences: true,
+        booleans: true,
       }, output: {
         comments: false,  // remove all comments
       },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
     }),
-    // new webpack.NoErrorsPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ["common", "manifest"]
-    }),
+    new webpack.optimize.CommonsChunkPlugin(
+      {
+        name: 'vendor', filename: 'vendor-[chunkhash:6].js',
+        name: 'bundle', filename: 'bundle-[chunkhash:6].js'
+      }
+    ),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    // new HappyPack({
-    //   loaders: ['babel?presets[]=es2015'],
-    // }),
+    new HappyPack({
+      loaders: [{
+        loader: 'babel-loader',
+      }],
+      threads: 4
+    }),
     new CopyWebpackPlugin([
       {from: path.join(__dirname, 'asserts') + '/**/*', to: path.join(__dirname, 'dist') + '/'},
       {from: path.join(__dirname, 'server.js'), to: path.join(__dirname, 'dist') + '/'},
       {from: path.join(__dirname, 'package.json'), to: path.join(__dirname, 'dist') + '/'},
-      {from: path.join(__dirname,'/server/controller/map.js'),to:path.join(__dirname,'dist')+'/controller'},
-      {from: path.join(__dirname,'fake/*'),to:path.join(__dirname,'dist')+'/fake'},
-      {from: path.join(__dirname,'config.json'),to:path.join(__dirname,'dist')+'/'},
+      {from: path.join(__dirname, '/server/controller/map.js'), to: path.join(__dirname, 'dist') + '/controller'},
+      {from: path.join(__dirname, 'fake/*'), to: path.join(__dirname, 'dist') + '/fake'},
+      {from: path.join(__dirname, 'config.json'), to: path.join(__dirname, 'dist') + '/'},
     ]),
 
     new ExtractTextPlugin("styles-[chunkhash:6].css"),
   ],
-  // resolve: {
-  //   extensions: ['', '.js', '.jsx']
-  // },
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        // loaders: ['happypack/loader'],
-        // loaders: ['babel'],
         exclude: /node_modules/,
         include: __dirname,
         use: [{
-          loader: "babel-loader",
+          // loader: "babel-loader",
+          loader: "happypack/loader",
         }]
       },
       {
-        // test: /\.less$/,
-        // loader: ExtractTextPlugin.extract('style-loader', "css-loader!less-loader")
-        //loader: ( "style-loader!css-loader!less-loader")
         test: /\.less$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
@@ -92,11 +98,10 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           use: ['css-loader']
         })
-        // loader: ExtractTextPlugin.extract('', "css-loader")
       },
       {
         test: /\.(png|jpe?g|eot|svg|ttf|woff2?)$/,
-        use:[{
+        use: [{
           loader: "url-loader?limit=8192&name=images/[name]-[hash:6].[ext]"
         }]
       },
