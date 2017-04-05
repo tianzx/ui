69c1aed65456ab5e513981d118b2682396ef9590
tianzx <tianzx@aliyun.com>
2017-04-05 16:42:53 +0800

    update webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -33,23 +33,23 @@ module.exports = {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   mangle: true,
-    //   compress: {
-    //     unused: true,
-    //     dead_code: true, // big one--strip code that will never execute
-    //     warnings: false, // good for prod apps so users can't peek behind curtain
-    //     drop_debugger: true,
-    //     conditionals: true,
-    //     evaluate: true,
-    //     drop_console: true, // strips console statements
-    //     sequences: true,
-    //     booleans: true,
-    //   }, output: {
-    //     comments: false,  // remove all comments
-    //   },
-    //   exclude: [/\.min\.js$/gi] // skip pre-minified libs
-    // }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        unused: true,
+        dead_code: true, // big one--strip code that will never execute
+        warnings: false, // good for prod apps so users can't peek behind curtain
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true, // strips console statements
+        sequences: true,
+        booleans: true,
+      }, output: {
+        comments: false,  // remove all comments
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
     // new webpack.optimize.UglifyJsPlugin({
     //   beautify: false,
     //   mangle: {
