7f9761a2cf9a84d0207b682b807b56ddeff9cf68
tianzx <tianzx@aliyun.com>
2017-04-11 17:24:52 +0800

    devops test

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HappyPack = require('happypack');
+// const HappyPack = require('happypack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: {
