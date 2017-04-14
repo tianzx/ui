26b94e6568afbf3e9233c6da936727b23f4ac852
tianzx <tianzx@aliyun.com>
2017-04-14 16:05:49 +0800

    optimize lodash

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,7 +10,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: {
     main: './src/index',
-    vendor: [ 'react','lodash'],
+    vendor: [ 'react'],
     // vendor2: ['antd']
   },
   output: {
