caedfd41260bf9e908efa7eb8e760e0287e58fda
tianzx <tianzx@aliyun.com>
2017-04-20 16:05:20 +0800

    optimize for production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,7 +10,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: {
     main: './src/index',
-    vendor: [ 'superagent'],
+    // vendor: [ 'superagent'],
     // vendor2: ['antd']
   },
   output: {
