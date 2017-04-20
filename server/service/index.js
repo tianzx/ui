5d135e5f4a0f5a484d58f1b1838e57c452467a2b
tianzx <tianzx@aliyun.com>
2017-04-20 09:19:48 +0800

    optimize for module

--- server/service/index.js
+++ server/service/index.js
@@ -1,3 +1,20 @@
 /**
  * Created by tianzx on 2017/4/19.
  */
+const fs = require('fs')
+
+function ScanDir(path,files) {
+  let that = this
+  if (fs.statSync(path).isFile()) {
+    return files.push(path)
+  }
+  try {
+    fs.readdirSync(path).forEach(function (file) {
+      ScanDir.call(that, path + '/' + file)
+    })
+  } catch (e) {
+  }
+}
+
+ScanDir(process.cwd())
+console.log(files)
---
+++ server/test/ioc/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by tianzx on 2017/4/19.
+ */
+// const scanDir = require('../../service/index');
+// let files = [];
+// filess = scanDir("/Volumes/Samsung_T3/workspace/ui/server/controller",files)
+// console.log(filess);
