943a6c70fec8f4c7bd5c40138bbba4139241c08e
tianzx <tianzx@aliyun.com>
2017-05-02 15:05:29 +0800

    convert npm to pnpm for increase travis compile speed

---
+++ server/test/file/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tianzx on 2017/4/28.
+ */
+const readChunk = require('read-chunk');
+const fileType = require('file-type');
+const buffer = readChunk.sync('/Volumes/Samsung_T3/workspace/ui/uploads/bff76b49a627905dd4ac0e9b5f3cf186.jpg', 0, 4100);
+
+console.log(fileType(buffer));
