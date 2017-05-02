0add1addacb92160029517edfdd6ab16719d5c72
tianzx <tianzx@aliyun.com>
2017-05-02 14:55:20 +0800

    convert npm to cnpm

--- server/controller/common.js
+++ server/controller/common.js
@@ -8,11 +8,34 @@ const storage = multer.diskStorage({
     cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
+    // cb(null, file.originalname)
+    cb(null, Date.now() + '_' + file.originalname)
+  },
+
 })
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  // 文件过滤,这里只能是图片文件，通过返回值判断是否成功，下面不用在设置！
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype !== 'image/png'
+      && file.mimetype !== 'image/jpg'
+      && file.mimetype !== 'image/jpeg'
+      && file.mimetype !== 'image/gif') {
+      // console.log('Got file of type', file.mimetype);
+      // console.log('false')
+      return cb(new Error('the file type cant be support'));
+    }else {
+      // console.log('true')
+      return cb(null, true);
+    }
+
+  },
+  limits: {
+    fieldNameSize: 50,
+    fileSize: 1024 * 1024
+  }
+}).single('file');
 
 const http = require('http');
 const request = require('request');
@@ -21,9 +44,17 @@ const qs = require('qs');
 
 const common = function (app) {
 
-  app.post('/api/upload', upload.single('file'), function (req, res) {
-    console.log(req.file);
-    res.json({'code': '200'});
+  app.post('/api/upload', function (req, res) {
+    upload(req, res, function (err) {
+      console.log(err)
+      if (err) {
+        // 发生错误
+        return res.json({'error': '出錯了,文件類型不對'});
+      }else{
+        return res.json({'code': '200'});
+      }
+    })
+    // console.log(req.file);
   });
 }
 
