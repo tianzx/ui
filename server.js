1fda2eba61866d9f36875d42a0c1ce0ddbba8863
tianzx <tianzx@aliyun.com>
2017-05-16 17:50:56 +0800

    solve cdn question

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (isDeveloping) {
     // etag: false,
     // extensions: ['htm', 'html'],
     // index: false,
-    maxAge: '0 days',
+    maxAge: '1 days',
     // redirect: false,
     // setHeaders: function (res, path, stat) {
     //   res.set('x-timestamp', Date.now());
--- src/views/User/index.js
+++ src/views/User/index.js
@@ -2,6 +2,7 @@
  * Created by tianzx on 16/8/25.
  */
 import React,{Component} from 'react';
+
 export default class User extends Component {
 
     render() {
