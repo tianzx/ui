3d0af9669a9bd45104bfa43c54c88816a497236a
tianzx <tianzx@aliyun.com>
2017-08-01 11:39:53 +0800

    update package.json when package build in docker

--- src/middlewares/messagePack.js
+++ src/middlewares/messagePack.js
@@ -1,16 +1,16 @@
 /**
  * Created by tianzx on 2017/5/15.
  */
-const msgpack = require("msgpack-lite");
-const isPromise = require('../utils/index')
-
-function msgpack_middleware({ getState }) {
-  return (next) => (action) => {
-    //
-    // const prevState = getState();
-    // console.log(prevState)
-
-    // return returnValue;
-  };
-}
-export default msgpack_middleware;
+// const msgpack = require("msgpack-lite");
+// const isPromise = require('../utils/index')
+//
+// function msgpack_middleware({ getState }) {
+//   return (next) => (action) => {
+//     //
+//     // const prevState = getState();
+//     // console.log(prevState)
+//
+//     // return returnValue;
+//   };
+// }
+// export default msgpack_middleware;
